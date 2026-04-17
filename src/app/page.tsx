f6c7da56d74af7d7f1948d29b21619489ae97ea3
jasonenkristo123 <jasonenkristo@gmail.com>
2026-04-17 20:30:55 +0700

    gsap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,33 @@
 "use client"
-import gsap from "gsap";
 import { useRef } from "react"
-import { useGSAP } from "@gsap/react";
-
+import AnimatedHeader from "./components/AnimatedHeader";
 
 export default function Home() {
 
   const container = useRef(null);
 
-
-
-  useGSAP(() => {
-    gsap.fromTo('.box', {
-      y: 0,
-    }, {
-      y: -100,
-      duration: 3,
-      repeat: -1,
-      yoyo: true,
-    })
-  }, {
-    scope: container
-  })
-
   return (
-    <main className="w-screen min-h-screen bg-slate-800 flex items-center justify-center">
-      <section ref={container} className="flex items-center justify-center gap-10">
-        <div className="rounded-full box bg-slate-400 w-24 h-24 relative">
-            <h1 className="text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-xl">
-              Ball 1
-            </h1>
-        </div>
+    <main className="w-screen min-h-screen ">
+      <section className="flex items-center justify-center h-screen bg-base-100">
+        <AnimatedHeader>
+          <h1 className="text-center text-base-200 ">
+            Lorem ipsum dolor sit
+          </h1>
+        </AnimatedHeader>
+      </section>
+      <section className="flex items-center justify-center h-screen bg-base-300">
+        <AnimatedHeader>
+          <h1 className="text-center text-base-400 ">
+            Lorem ipsum dolor sit
+          </h1>
+        </AnimatedHeader>
+      </section>
+      <section className="flex items-center justify-center h-screen bg-base-500">
+        <AnimatedHeader>
+          <h1 className="text-center text-base-600 ">
+            Lorem ipsum dolor sit
+          </h1>
+        </AnimatedHeader>
       </section>
     </main>
   );
