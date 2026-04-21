7a2cf8a5b85eacaa5229fba8451a4938f7574ac5
jasonenkristo123 <jasonenkristo@gmail.com>
2026-04-21 13:03:39 +0700

    feat: finished message section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 "use client"
 
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Navbar from "./components/Navbar";
 import HeroSection from "./sections/HeroSection";
+import { gsap } from "gsap";
+import MessageSection from "./sections/MessageSection";
 
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <HeroSection />
+      <MessageSection />
     </main>
   );
 }
--- src/app/sections/HeroSection.tsx
+++ src/app/sections/HeroSection.tsx
@@ -29,7 +29,23 @@ export default function HeroSection() {
             yPercent: 200,
             stagger: 0.02,
             ease: "power2.out"
-        }, "-=0.5")
+        }, "-=0.5");
+
+        const heroTl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".hero-container",
+                start: "1% top",
+                end: "bottom top",
+                scrub: true,
+            }
+        });
+
+        heroTl.to(".hero-container", {
+            rotate: 7,
+            scale: 0.9,
+            yPercent: 30,
+            ease: "power1.inOut"
+        })
     })
 
     return (
