85ee07d1d6f77ad53fbe8c6a52d8b88d0ef8e711
jasonenkristo123 <jasonenkristo@gmail.com>
2026-05-01 21:17:26 +0700

    fix the bug

--- src/app/sections/TestimonialSection.tsx
+++ src/app/sections/TestimonialSection.tsx
@@ -22,81 +22,83 @@ export default function TestimonialSection() {
     };
 
     useGSAP(() => {
-        gsap.set(".testimonial-section", {
-            marginTop: "-140vh"
+        gsap.set(".testimonials-section", {
+            marginTop: "-140vh",
         });
 
         const tl = gsap.timeline({
             scrollTrigger: {
-                trigger: ".testimonial-section",
+                trigger: ".testimonials-section",
                 start: "top bottom",
                 end: "200% top",
-                scrub: true
-            }
+                scrub: true,
+            },
         });
 
-        tl.to(".testimonial-section .first-title", {
+        tl.to(".testimonials-section .first-title", {
             xPercent: 70,
-        }, "<").to(".testimonial-section .second-title", {
-            xPercent: 25,
-        }, "<").to(".testimonial-section .third-title", {
-            xPercent: -50,
-        }, "<");
+        })
+            .to(
+                ".testimonials-section .sec-title",
+                {
+                    xPercent: 25,
+                },
+                "<"
+            )
+            .to(
+                ".testimonials-section .third-title",
+                {
+                    xPercent: -50,
+                },
+                "<"
+            );
 
         const pinTl = gsap.timeline({
             scrollTrigger: {
-                trigger: "testimonial-section",
+                trigger: ".testimonials-section",
                 start: "10% top",
                 end: "200% top",
                 scrub: 1.5,
                 pin: true,
-            }
+            },
         });
 
         pinTl.from(".vd-card", {
             yPercent: 150,
             stagger: 0.2,
             ease: "power1.inOut",
-        })
-    })
+        });
+    });
 
     return (
-        <section className="testimonial-section">
-            <div className="absolute size-full flex flex-col items-center pt-[15vw]">
-                <h1 className="text-black first-title">
-                    What&apos;s
-                </h1>
-                <h1 className="text-light-brown second-title">
-                    Everyone
-                </h1>
-                <h1 className="text-black third-title">
-                    Saying
-                </h1>
-
-                <div className="pin-box">
-                    {
-                        cards.map((card, index) => (
-                            <div key={index} className={`vd-card ${card.translation} ${card.rotation}`}>
-                                <video 
-                                    src={card.src} 
-                                    playsInline 
-                                    muted 
-                                    loop 
-                                    ref={(el) => {
-                                        if(el) vdRef.current[index] = el;
-                                    }}
-                                    className="size-full object-cover" 
-                                    onMouseEnter={() => handlePlay(index)}
-                                    onMouseLeave={() => handlePause(index)}
-                                />
-                                
-
-                            </div>
-                        ))
-                    }
-                </div>
+        <section className="testimonials-section">
+            <div className="absolute size-full flex flex-col items-center pt-[5vw]">
+                <h1 className="text-black first-title">What&apos;</h1>
+                <h1 className="text-light-brown sec-title">Everyone</h1>
+                <h1 className="text-black third-title">Talking</h1>
             </div>
-            
+
+            <div className="pin-box">
+                {cards.map((card, index) => (
+                    <div
+                        key={index}
+                        className={`vd-card ${card.translation} ${card.rotation}`}
+                        onMouseEnter={() => handlePlay(index)}
+                        onMouseLeave={() => handlePause(index)}
+                    >
+                        <video
+                            ref={(el) => {
+                                if (el) vdRef.current[index] = el;
+                            }}
+                            src={card.src}
+                            playsInline
+                            muted
+                            loop
+                            className="size-full object-cover"
+                        />
+                    </div>
+                ))}
+            </div>
         </section>
     )
 }
